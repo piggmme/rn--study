631010373648e4a88707024ed802fc2dbfc71748
cherry.a <cherry.a@kakaocorp.com>
2024-11-22 15:07:12 +0900

    eslint 설정

---
+++ eslint.config.mjs
@@ -0,0 +1,44 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  stylistic.configs.customize({
+    semi: false,
+  }),
+  {
+    rules: {
+      '@stylistic/function-call-spacing': 'error',
+      '@stylistic/brace-style': [
+        'error',
+        '1tbs',
+        { allowSingleLine: true },
+      ],
+      '@stylistic/jsx-one-expression-per-line': 'off',
+      '@stylistic/object-curly-newline': [
+        'error',
+        {
+          multiline: true,
+          consistent: true,
+          minProperties: 5,
+        },
+      ],
+      '@stylistic/array-element-newline': [
+        'error',
+        {
+          ArrayExpression: 'consistent',
+          ArrayPattern: { minItems: 3 },
+        },
+      ],
+      '@stylistic/jsx-quotes': ['error', 'prefer-single'],
+      '@stylistic/space-before-function-paren': ['error', 'always'],
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+]
